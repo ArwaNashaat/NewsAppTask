b1299c9f0efe684c426fdf1bf65d564137b5cadf
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-01 16:36:28 +0200

    1) Refactor register funtion 2) Change functions to private

--- Api/app/Http/Controllers/UserController.php
+++ Api/app/Http/Controllers/UserController.php
@@ -2,31 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Hash;
 use App\Mail\SendEmail;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
+
+        $userData = $this->validateRequest($request);
+        $user = User::create($userData);
+        $this->sendEmail($user->password);
+        return $user->password;
+    }
+
+    private function sendEmail($password) {
+        $data = array(
+            'password' => $password
+        );
+        Mail::to('arwanashaat@gmail.com')->send(new SendEmail($data));
+    }
+
+    private function validateRequest(Request $request){
         $validatedData = $request->validate([
             'name'=>'required',
             'email'=>'email|required|unique:users',
             'dateOfBirth' => 'required',
             'password'=>'required',
-            ]);      
-        $user = User::create($validatedData);
-        $this->sendEmail($request->password);
-        return $user;
-    }
-
-    public function sendEmail($password) {
-        $data = array(
-            'password' => $password
-        );
-        
-        Mail::to('arwanashaat@gmail.com')->send(new SendEmail($data));
+            ]);   
+        $validatedData['password'] =   Hash::make(Str::random(16)); 
+        return $validatedData;
     }
 }
