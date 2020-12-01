4823b93b036372631dd397dd61f388ceac319fd5
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-01 12:27:57 +0200

    Create post request

--- FrontEnd/src/components/RegisterComponent.js
+++ FrontEnd/src/components/RegisterComponent.js
@@ -1,25 +1,27 @@
+import axios from 'axios';
 const { Component } = require("react");
+
 
  class RegisterComponent extends Component{
     constructor(props){
         super(props)
         this.state = {
-            username: "Name",
+            name: "Name",
             email: "Email",
-            DOB: "Date of Birth"
+            dateOfBirth: "Date of Birth",
+            password:"password"
         }
         this.handleChange = this.handleChange.bind(this);
         this.register = this.register.bind(this);
-
     }
     render(){
         return(
         <div className="registerComponent">
-            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+            User Name: <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
             <br></br>
             User Email: <input type="text" name="email" value={this.state.email} onChange={this.handleChange}/>
             <br></br>
-            Date of Birth: <input type="text" name="DOB" value={this.state.DOB} onChange={this.handleChange}/>
+            Date of Birth: <input type="text" name="dateOfBirth" value={this.state.dateOfBirth} onChange={this.handleChange}/>
             <br></br>
             <button onClick={this.register}>Register</button>
         </div>
@@ -32,7 +34,21 @@ const { Component } = require("react");
         });
     }
 
-    register(){
+    register(e){
+        this.verifyEmail();
+        e.preventDefault();
+        console.log(this.state);
+        
+        axios.post('http://localhost:8001/api/register', this.state, {
+            headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
+        })
+        .then(respone =>{
+            console.log(respone);
+        })
+        .catch(error => console.log(error))
+    }
+
+    verifyEmail(){
         if (this.state.email !== "undefined") {
             var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
             if (!pattern.test(this.state.email)) {
@@ -40,6 +56,7 @@ const { Component } = require("react");
             }
           }
     }
+   
 }
 
 
