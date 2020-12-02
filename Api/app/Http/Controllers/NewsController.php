734bbc10b456d5ae1bd02eb57f99e91a50103974
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-02 17:24:45 +0200

    Create getNewsFrom function

---
+++ Api/app/Http/Controllers/NewsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getNewsFrom($country)
+    {
+        $httpClient = new Client();
+        $url = 'https://newsapi.org/v2/top-headlines?country='.$country.'&apiKey='.env('NEWS_API_KEY', '');
+    
+        $response = $httpClient->get($url);
+        $body = json_decode($response->getBody());
+
+        return response()->json(['news'=> $body], 200);
+    }
+
+}
