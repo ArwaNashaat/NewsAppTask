aa978e1ac3f1b3cb0e432e8e28f38552cc956004
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-02 21:23:47 +0200

    Create news route

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import RegisterComponent from './components/RegisterComponent'
 import LoginComponent from './components/LoginComponent'
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import NewsComponent from './components/NewsComponent'
+import {BrowserRouter, Route} from 'react-router-dom';
 const { Component } = require("react");
 
 class App extends Component {
@@ -10,23 +11,16 @@ class App extends Component {
 
       <BrowserRouter>
         <div className="App">
-          <Route path='/' component={RegisterComponent} exact={true}/>
-          <Route path='/Login' component={LoginComponent} exact={true}/>
+          <Route path='/' exact component={RegisterComponent} />
+          <Route path='/Login' exact component={LoginComponent} />
+          <Route path='/News' exact component={NewsComponent} />
+          <Route component={RegisterComponent}/>
         </div>
       </BrowserRouter>
 
     );
   }
 }
-
-
-const routes=(
-  <BrowserRouter>
-    <div>
-      <Route path='/login' component={LoginComponent}/>
-    </div>
-  </BrowserRouter>
-)
 
 export default App;
 
