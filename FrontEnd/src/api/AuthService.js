d61d112ddf2cb1706e6f8160c1305755c9e73b5c
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-03 00:18:47 +0200

    Modify getNews to get both business and sports news

--- FrontEnd/src/api/AuthService.js
+++ FrontEnd/src/api/AuthService.js
@@ -13,6 +13,17 @@ class AuthService{
             headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
         })
     }
+
+    getNews($url, $category){
+        axios.all([
+            axios.get($url+'/'+$category[0]),
+            axios.get($url+'/'+$category[1])
+          ])
+          .then(axios.spread((businessNews, sportNews) => {
+            console.log('Business News: ', businessNews);
+            console.log('Sport News: ', sportNews);
+          }));
+    }
 }
 
 export default new AuthService();
--- FrontEnd/src/components/NewsComponent.js
+++ FrontEnd/src/components/NewsComponent.js
@@ -1,3 +1,4 @@
+import AuthService from '../api/AuthService.js'
 import axios from 'axios';
 
 const { Component } = require("react");
@@ -6,7 +7,8 @@ class NewsComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            country: "eg"
+            country: "eg",
+            category: ["business", "sports"]
         }
         this.handleClick = this.handleClick.bind(this);
     }
@@ -31,15 +33,25 @@ class NewsComponent extends Component {
     getNews(event) {
         event.preventDefault();
         this.url = 'http://localhost:8001/api/news/' + this.state.country;
+        AuthService.getNews(this.url, this.state.category)
+        // axios.all([
+        //     axios.get(this.url+'/'+this.state.categoryBusiness),
+        //     axios.get(this.url+'/'+this.state.categorySports)
+        //   ])
+        //   .then(axios.spread((businessNews, sportNews) => {
+        //     console.log('Business News: ', businessNews);
+        //     console.log('Sport News: ', sportNews);
+        //   }));
 
-        axios.get(this.url)
-            .then((response) => {
-                console.log(response.data);
-                console.log(response.status);
-                console.log(response.statusText);
-                console.log(response.headers);
-            });
-        }
+        // axios.all([
+        //     axios.get($url+'/'+$category[0]),
+        //     axios.get($url+'/'+$category[1])
+        //   ])
+        //   .then(axios.spread((businessNews) => {
+        //     console.log('Business News: ', businessNews[0]);
+        //     console.log('Sport News: ', businessNews[1]);
+        //   }));
+    }
 }
 
 export default NewsComponent;
