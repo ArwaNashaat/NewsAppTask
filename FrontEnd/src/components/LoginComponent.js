c91063a5b60fed6a1d552409f7eabcdea8d63681
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-02 21:24:51 +0200

    Add Route to news on Successful login

--- FrontEnd/src/components/LoginComponent.js
+++ FrontEnd/src/components/LoginComponent.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 const { Component } = require("react");
 
 class LoginComponent extends Component {
+    
+
     constructor(props){
         super(props)
         this.state = {
             email: "Email",
             password: "****",
+            redirect: "/News",
+
         }
         this.handleChange = this.handleChange.bind(this);
         this.login = this.login.bind(this);
@@ -21,9 +25,7 @@ class LoginComponent extends Component {
                 <input type="text" name="email" value={this.state.email} onChange={this.handleChange} /><br></br><br></br>
 
                 <label for="password">Password:</label><br></br>
-                <input type="text" name="password" value={this.state.password} onChange={this.handleChange} /><br></br><br></br>
-
-                
+                <input type="password" name="password" value={this.state.password} onChange={this.handleChange} /><br></br><br></br>
 
                 <button onClick={this.login}>Login</button>
             </form>
@@ -37,14 +39,14 @@ class LoginComponent extends Component {
     }
 
     login(e){
-        // this.verifyEmail();
+
         e.preventDefault();
         
         axios.post('http://localhost:8001/api/login', this.state, {
             headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
         })
         .then(respone =>{ 
-            console.log(respone);
+            this.props.history.push("/News");
         })
         .catch(error => alert("Wrong Email or Password"))
     }
