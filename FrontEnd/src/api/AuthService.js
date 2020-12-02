83ea2a1da3908080f04550e309a0a87227ba275b
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-02 23:10:47 +0200

    Move authentication axios request to AuthService

---
+++ FrontEnd/src/api/AuthService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+class AuthService{
+    
+     login($userCredentials){
+        return axios.post('http://localhost:8001/api/login', $userCredentials, {
+            headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
+        })
+    }
+
+    register($userData){
+        return axios.post('http://localhost:8001/api/register', $userData, {
+            headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
+        })
+    }
+}
+
+export default new AuthService();
