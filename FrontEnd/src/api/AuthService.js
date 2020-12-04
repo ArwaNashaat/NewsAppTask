b000e1bc38b38960b69c044f781db8815e28c688
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-04 21:57:30 +0200

    Create axios baseUrl

--- FrontEnd/src/api/AuthService.js
+++ FrontEnd/src/api/AuthService.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 class AuthService{
     
-     login(userCredentials){
-        return axios.post('http://localhost:8001/api/login', userCredentials, {
+    login(userCredentials){
+        return axios.post('api/login', userCredentials, {
             headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
         })
     }
 
     register(userData){
-        return axios.post('http://localhost:8001/api/register', userData, {
+        return axios.post('api/register', userData, {
             headers: {'Content-Type': 'application/json', 'Accept':'application/json'}
         })
     }
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './App';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8001/';
 ReactDOM.render(<App/>, document.getElementById("root"));
