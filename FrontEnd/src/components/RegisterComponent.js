952cd2f9fe227be5b0bee05631b01f8f6c086221
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-04 22:11:02 +0200

    Refactor alert messages

--- FrontEnd/src/components/RegisterComponent.js
+++ FrontEnd/src/components/RegisterComponent.js
@@ -2,7 +2,7 @@ import AuthService from '../api/AuthService.js'
 const { Component } = require("react");
 
 
- class RegisterComponent extends Component{
+class RegisterComponent extends Component{
     constructor(props){
         super(props)
         this.state = {
@@ -40,14 +40,17 @@ const { Component } = require("react");
     }
 
     register(event){
-        this.verifyEmail();
+        if(!this.verifyEmail()){
+            alert("Wrong Email");
+            return;
+        }
         event.preventDefault();
         
         AuthService.register(this.state)
         .then(respone =>{
             alert("Successfully Registered, please login");
         })
-        .catch(error => alert("Please Check That You Entered Correct Email"))
+        .catch(error => alert("Please Enter Your Data Correctly"))
     }
 
     verifyEmail(){
@@ -55,9 +58,11 @@ const { Component } = require("react");
             var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
             
             if (!pattern.test(this.state.email)) {
-              alert("Enter a valid email")
+                return false;
             }
-          }
+
+            return true;
+        }
     }
 }
 
