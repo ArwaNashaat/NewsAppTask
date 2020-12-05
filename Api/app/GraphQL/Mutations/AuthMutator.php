fdc84d430a3570fc50f155d2715740390bb9c79e
Arwa Nashaat <arwanashaat@gmail.com>
2020-12-06 00:46:26 +0200

    1) Validate login data 2) Create register() in AuthMutator

--- Api/app/GraphQL/Mutations/AuthMutator.php
+++ Api/app/GraphQL/Mutations/AuthMutator.php
@@ -2,23 +2,58 @@
 
 namespace App\GraphQL\Mutations;
 
+use Illuminate\Support\Str;
 use Tymon\JWTAuth\Facades\JWTAuth;
-use GraphQL\Type\Definition\ResolveInfo;
-use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendEmail;
 
-class AuthMutator{
-    public function login($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+class AuthMutator
+{
+
+    public function login($rootValue, array $args)
     {
         $credentials = [
-                'email' => $args['email'],
-                'password' => $args['password']
-            ];
-        
+            'email' => $args['email'],
+            'password' => $args['password']
+        ];
+
         $token = JWTAuth::attempt($credentials);
         if (!$token) {
             return response()->json(['error' => 'Wrong Email or Password'], 401);
         }
 
         return $token;
+    }
+
+    public function register($rootValue, array $args)
+    {
+        $userData = [
+            'name' => $args['name'],
+            'email' => $args['email'],
+            'dateOfBirth' => $args['dateOfBirth']
+        ];
+
+        $userData['password'] = $this->autoGeneratePassword();
+        
+        $this->sendEmail($userData);
+
+        $userData['password'] = bcrypt($userData['password']);
+        
+        $user = User::create($userData);
+        return json_decode($user);
+    }
+
+    private function autoGeneratePassword()
+    {
+        return Str::random(16);
+    }
+
+    private function sendEmail($user)
+    {
+        $password = array(
+            'password' => $user['password']
+        );
+        Mail::to($user['email'])->send(new SendEmail($password));
     }
 }
